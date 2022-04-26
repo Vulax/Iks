134b00d29be648b7533272edd07343f3ea6c45c5
Vulax <vukajlotodorovic@gmail.com>
2022-04-26 10:55:23 +0200

    Message box

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function show() {
     
         document.getElementById('image').style.display = "block";
-         
+        document.getElementById('green').style.display = "block";
+        document.getElementById('op').style.display = "block";
+}
+
+function hide(){
+        document.getElementById('image').style.display = "none";  
 }
